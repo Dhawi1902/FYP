c2b18195a54cb503c07ba2bfc2d2cb5a6e37fcbb
dhawi <dhawi.ars02@gmail.com>
2024-10-10 22:54:19 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FYP
+version https://git-lfs.github.com/spec/v1
+oid sha256:eecdb857611f9087de710d01947972f12ec9eb1cd5695d07c513d34a404baa89
+size 5
--- try.py
+++ try.py
@@ -1,3 +1,3 @@
-import sys
-print(sys.version)
-import cv2
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d3db7f83d6cbcf38a6e313090ddb34313660d3d98b5fa9930cce7b5c14a769e
+size 40
