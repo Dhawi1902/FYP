e84ebf5cb9ea0e1b0cb2e8d7dc2a2c3bf3fbdfa8
dhawi <dhawi.ars02@gmail.com>
2024-11-05 10:57:34 +0800

    update

--- static/js/main.js
+++ static/js/main.js
@@ -54,4 +54,14 @@ $(document).ready(function () {
         });
     });
 
+    // JavaScript for changing navbar background on scroll
+    window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('navbar-scrolled');
+        } else {
+            navbar.classList.remove('navbar-scrolled');
+        }
+    });
+
 });
